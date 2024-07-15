77495e98358a5fd68865d6d6fc12143f6c938b66
Ba1Kerrr <daniildavidov2707@gmail.com>
2024-07-16 00:47:34 +0300

    edited and comleted

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
-from flask import render_template,url_for
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
-
+@app.route('/')
+def Home():
+    return render_template('index.html')
+@app.route('/About/')
+def About():
+    return render_template('About.html')
+@app.route('/Contact/')
+def Contact():
+    return render_template('Contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
